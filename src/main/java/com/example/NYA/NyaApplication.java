95ae397aa87f1e21edb811b20b88edc57793c37b
nozawa_tomoya <nozawa.tomoya@alhinc.jp>
2025-10-06 16:07:50 +0900

    環境構築

---
+++ src/main/java/com/example/NYA/NyaApplication.java
@@ -0,0 +1,13 @@
+package com.example.NYA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NYA/NyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NYA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
