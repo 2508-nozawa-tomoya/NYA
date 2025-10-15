a3662abc7fc062dcbc44a10dc9cef99c9ddd90e7
sato_issei <sato.issei@alhinc.jp>
2025-10-15 16:15:46 +0900

    レイアウト修正（勤怠登録画面、勤怠編集画面）

--- src/main/java/com/example/NYA/controller/AddController.java
+++ src/main/java/com/example/NYA/controller/AddController.java
@@ -1,8 +1,10 @@
 package com.example.NYA.controller;
 
 import com.example.NYA.controller.form.AttendanceForm;
+import com.example.NYA.controller.form.UserForm;
 import com.example.NYA.security.LoginUserDetails;
 import com.example.NYA.service.AttendanceService;
+import com.example.NYA.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -14,14 +16,36 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 @Controller
 public class AddController {
     @Autowired
     AttendanceService attendanceService;
+    @Autowired
+    UserService userService;
 
     @GetMapping("/new")
-    public String showAddForm(Model model) {
+    public String showAddForm(@AuthenticationPrincipal LoginUserDetails loginUser,
+                              Model model) {
+
+        //所定労働時間の計算
+        UserForm user = userService.findById(loginUser.getId());
+        LocalTime workStart = user.getWorkStart();
+        LocalTime workEnd = user.getWorkEnd();
+        LocalTime restStart = user.getRestStart();
+        LocalTime restEnd = user.getRestEnd();
+
+        Duration workTime = Duration.between(workStart, workEnd);
+        Duration restTime = Duration.between(restStart, restEnd);
+
+        workTime = workTime.minus(restTime);
+        String defaultWorkTime = attendanceService.formatDuration(workTime);
+
         model.addAttribute("attendanceForm", new AttendanceForm());
+        model.addAttribute("loginUser", loginUser);
+        model.addAttribute("defaultWorkTime", defaultWorkTime);
         return "new";
     }
 
@@ -34,7 +58,23 @@ public class AddController {
         // 入力内容に関するバリデーション
         if (result.hasErrors()) {
             ModelAndView mav = new ModelAndView("new");
+
+            //所定労働時間の計算
+            UserForm user = userService.findById(loginUser.getId());
+            LocalTime workStart = user.getWorkStart();
+            LocalTime workEnd = user.getWorkEnd();
+            LocalTime restStart = user.getRestStart();
+            LocalTime restEnd = user.getRestEnd();
+
+            Duration workTime = Duration.between(workStart, workEnd);
+            Duration restTime = Duration.between(restStart, restEnd);
+
+            workTime = workTime.minus(restTime);
+            String defaultWorkTime = attendanceService.formatDuration(workTime);
+
             mav.addObject("attendanceForm", attendanceForm);
+            mav.addObject("loginUser", loginUser);
+            mav.addObject("defaultWorkTime", defaultWorkTime);
             return mav;
         }
 
